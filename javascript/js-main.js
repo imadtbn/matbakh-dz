acb4182367614cf8e0d8729ef0fed66993191eb2
imad tbn <tabaniimadeddine@gmail.com>
2026-03-09 13:20:32 +0100

    Update js-main.js

--- javascript/js-main.js
+++ javascript/js-main.js
@@ -173,3 +173,31 @@ deferredPrompt = null;
 });
 });
 });
+
+
+// بدء التحميل زر المزيد
+
+let visibleRecipes = 4;
+
+function renderRecipes(recipes) {
+
+    const container = document.getElementById("algerien-cards");
+    container.innerHTML = "";
+
+    recipes.slice(0, visibleRecipes).forEach(recipe => {
+        container.innerHTML += createRecipeCard(recipe);
+    });
+
+    if (visibleRecipes >= recipes.length) {
+        document.getElementById("loadMoreBtn").style.display = "none";
+    }
+
+}
+
+document.getElementById("loadMoreBtn").addEventListener("click", function() {
+
+    visibleRecipes += 4;
+
+    renderRecipes(allRecipes);
+
+});
